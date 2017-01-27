5c00fc9f27b78b6aa70f402761cfe4795682a6a3
Vianey_Narváez <Vianey_Narvez@192.168.1.70>
2017-01-26 22:51:41 -0600

    Leticia Vianey Narvaez Reyes A01410432 Ingenieria industrial y de sistemas Tarea 01. Problema 1

---
+++ src/spp2/lnarvaezr/t01/p1/SPP2LNarvaezRT01P1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spp2.lnarvaezr.t01.p1;
+
+/**
+ *
+ * @author Vianey_Narváez
+ */
+public class SPP2LNarvaezRT01P1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
